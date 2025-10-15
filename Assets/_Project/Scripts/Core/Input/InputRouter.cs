35bef73ce5fe3c14cedd9066f3679c7d7953f9fe
thisman <tablescable@gmail.com>
2025-10-15 10:40:01 +0500

    Add base Input System settings

---
+++ Assets/_Project/Scripts/Core/Input/InputRouter.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public enum GameMode { Gameplay, Inventory, Dialog, Paused }
+
+public class InputRouter : MonoBehaviour
+{
+    [SerializeField] private InputActionAsset _actions;
+
+    void Start() => SetMode(GameMode.Gameplay);
+
+    public void SetMode(GameMode mode)
+    {
+        _actions.bindingMask = null;
+
+        foreach (var m in _actions.actionMaps) m.Disable();
+
+        switch (mode)
+        {
+            case GameMode.Gameplay:
+                _actions.FindActionMap("Player").Enable();
+                _actions.FindActionMap("Interact").Enable();
+                break;
+            case GameMode.Inventory:
+            case GameMode.Dialog:
+            case GameMode.Paused:
+                _actions.FindActionMap("UI").Enable();
+                break;
+        }
+    }
+}
