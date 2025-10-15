dca195741d20d6eaf11383cb5b737e6fd9a54263
thisman <tablescable@gmail.com>
2025-10-16 00:20:16 +0500

    Update InteractableObject_Default sprites

--- Assets/_Project/Scripts/Data/Objects/InteractableInfo.cs
+++ Assets/_Project/Scripts/Data/Objects/InteractableInfo.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 [Serializable]
 public struct InteractableInfo
 {
-    public string DisplayName;          // "Сундук", "Рычаг", "Алтарь"
-    public string Description;          // "Открыть", "Активировать", "Помолиться"
-    public Sprite Icon;                 // иконка для UI
-    public InteractionType Type;        // тип (Use, Pickup, Talk, Examine и т.п.)
-    public bool RequiresCondition;      // есть ли условие (ключ, энергия, квест)
-    public float InteractionDistance;   // радиус/дистанция активации
-    public Color HighlightColor;        // цвет подсветки при наведении
+    public string DisplayName;
+    public string Description;
+    public InteractionType Type;
+    public bool RequiresCondition;
+    public float InteractionDistance;
+    public Color HighlightColor;
 }
