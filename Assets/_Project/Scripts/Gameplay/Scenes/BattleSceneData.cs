7483fee88cc90409f38736438a8c9d2c8070c88c
Алексей Леготкин <tablescable@gmail.com>
2025-10-18 08:53:24 +0500

    Add support for passing data to additive scenes

---
+++ Assets/_Project/Scripts/Gameplay/Scenes/BattleSceneData.cs
@@ -0,0 +1,14 @@
+using System;
+public sealed class BattleSceneData
+{
+    public UnitModel Hero { get; }
+    public ArmyModel Army { get; }
+    public UnitModel Enemy { get; }
+
+    public BattleSceneData(UnitModel hero, ArmyModel army, UnitModel enemy)
+    {
+        Hero = hero ?? throw new ArgumentNullException(nameof(hero));
+        Army = army ?? throw new ArgumentNullException(nameof(army));
+        Enemy = enemy ?? throw new ArgumentNullException(nameof(enemy));
+    }
+}
--- Assets/_Project/Scripts/Gameplay/Scenes/BattleSceneManager.cs
+++ Assets/_Project/Scripts/Gameplay/Scenes/BattleSceneManager.cs
@@ -8,6 +8,21 @@ public class BattleSceneManager : MonoBehaviour
 
     [Inject] private SceneLoader _sceneLoader;
 
+    private void Start()
+    {
+        string sceneName = gameObject.scene.name;
+        if (_sceneLoader != null && _sceneLoader.TryGetScenePayload<BattleSceneData>(sceneName, out var data))
+        {
+            string heroName = data.Hero?.Definition ? data.Hero.Definition.name : "<null>";
+            string enemyName = data.Enemy?.Definition ? data.Enemy.Definition.name : "<null>";
+            Debug.Log($"[BattleSceneManager] Hero: {heroName}, Army slots: {data.Army?.MaxSlots ?? 0}, Enemy: {enemyName}");
+        }
+        else
+        {
+            Debug.LogWarning("[BattleSceneManager] Unable to retrieve battle scene data payload");
+        }
+    }
+
     private void OnEnable()
     {
         _leaveBattleButton.onClick.AddListener(OnLeaveBattleButtonClicked);
@@ -20,6 +35,6 @@ public class BattleSceneManager : MonoBehaviour
 
     private async void OnLeaveBattleButtonClicked()
     {
-        await _sceneLoader.UnloadAdditiveAsync("Battle", "Cave_Level_1");
+        await _sceneLoader.CloseAdditiveWithDataAsync("Battle", null, "Cave_Level_1");
     }
 }
