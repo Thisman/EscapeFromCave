01e0a009114fed0ee06744ca7fcced28640fd44d
Алексей Леготкин <tablescable@gmail.com>
2025-10-18 17:43:43 +0500

    Register BattleScene UI layers via inspector

--- Assets/_Project/Scripts/Gameplay/Scenes/BattleSceneManager.cs
+++ Assets/_Project/Scripts/Gameplay/Scenes/BattleSceneManager.cs
@@ -5,15 +5,19 @@ using VContainer;
 public class BattleSceneManager : MonoBehaviour
 {
     [SerializeField] private Button _leaveBattleButton;
+    [SerializeField] private LayerRegistration[] _layers;
 
     [Inject] private SceneLoader _sceneLoader;
     [Inject] private StateMachine<BattleStateContext> _stateMachine;
     [Inject] private TacticState _tacticState;
     [Inject] private FightState _fightState;
     [Inject] private FinishState _finishState;
+    [Inject] private PanelController _panelController;
 
     private void Start()
     {
+        RegisterLayers();
+
         string sceneName = gameObject.scene.name;
         if (_sceneLoader != null && _sceneLoader.TryGetScenePayload<BattleSceneLoadingPayload>(sceneName, out var data))
         {
@@ -64,5 +68,39 @@ public class BattleSceneManager : MonoBehaviour
         }
 
         _stateMachine.SetState<TacticState>();
+    }
+
+    private void RegisterLayers()
+    {
+        if (_panelController == null || _layers == null)
+        {
+            return;
+        }
+
+        foreach (var layer in _layers)
+        {
+            if (layer == null)
+            {
+                continue;
+            }
+
+            var layerName = layer.LayerName;
+            if (string.IsNullOrEmpty(layerName))
+            {
+                continue;
+            }
+
+            _panelController.Register(layerName, layer.Elements);
+        }
+    }
+
+    [System.Serializable]
+    private class LayerRegistration
+    {
+        [SerializeField] private string _layerName;
+        [SerializeField] private GameObject[] _elements;
+
+        public string LayerName => _layerName;
+        public GameObject[] Elements => _elements;
     }
 }
