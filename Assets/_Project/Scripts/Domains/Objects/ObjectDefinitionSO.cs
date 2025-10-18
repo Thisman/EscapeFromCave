beb61036c86f71cc63721aae5408fd2f5e82135e
thisman <tablescable@gmail.com>
2025-10-18 12:04:35 +0500

    LF endings fix

---
+++ Assets/_Project/Scripts/Domains/Objects/ObjectDefinitionSO.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ObjectDefinitionSO", menuName = "Scriptable Objects/ObjectDefinitionSO")]
+public class ObjectDefinitionSO : ScriptableObject
+{
+    public Sprite Icon;
+
+    public string ObjectName;
+}
