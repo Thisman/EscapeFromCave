a8b5548177197206cb4fb7abbcaf657dbdde9518
thisman <tablescable@gmail.com>
2025-10-19 12:29:17 +0500

    First dialog

--- Assets/_Project/Scripts/Gameplay/Effects/ShowDialogEffect.cs
+++ Assets/_Project/Scripts/Gameplay/Effects/ShowDialogEffect.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Gameplay/Effects/Show Dialog")]
+[CreateAssetMenu(menuName = "Gameplay/Effects/Dialog")]
 public sealed class ShowDialogEffect : EffectSO, IAsyncEffect
 {
     [TextArea]
