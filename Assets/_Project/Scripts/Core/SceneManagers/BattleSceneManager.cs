3660bf4a2b01945ac755b768aadbaeb1c8d76d14
Алексей Леготкин <tablescable@gmail.com>
2025-10-22 11:28:25 +0500

    Add debug battle squad setup for scene manager

--- Assets/_Project/Scripts/Core/SceneManagers/BattleSceneManager.cs
+++ Assets/_Project/Scripts/Core/SceneManagers/BattleSceneManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using VContainer;
 
@@ -12,6 +13,8 @@ public class BattleSceneManager : MonoBehaviour
     private CombatLoopMachine _combatLoop;
     private BattlePhaseMachine _phaseMachine;
     private PanelController _panelController;
+
+    private readonly List<BattleSquadModel> _debugBattleSquads = new();
 
     [SerializeField] private BattleGridController _battleGridController;
     [SerializeField] private BattleGridDragAndDropController _battleGridDragAndDropController;
@@ -35,6 +38,56 @@ public class BattleSceneManager : MonoBehaviour
         _phaseMachine = new BattlePhaseMachine(_ctx, _combatLoop);
 
         _phaseMachine.Fire(BattleTrigger.Start);
+    }
+
+#if UNITY_EDITOR
+    [ContextMenu("Debug/Create Test Battle Squads")]
+#endif
+    private void DebugCreateTestBattleSquads()
+    {
+        _debugBattleSquads.Clear();
+
+        var playerUnit = new UnitModel(CreateDebugDefinition("Debug Hero", UnitType.Hero, 1, 120, 20, 10, 12, 3.5f), 1);
+        var allySquadOne = new SquadModel(CreateDebugDefinition("Debug Ally 1", UnitType.Ally, 1, 80, 12, 6, 10, 3f), 5);
+        var allySquadTwo = new SquadModel(CreateDebugDefinition("Debug Ally 2", UnitType.Ally, 1, 70, 14, 4, 11, 3.2f), 4);
+        var enemyUnit = new UnitModel(CreateDebugDefinition("Debug Enemy", UnitType.Enemy, 1, 150, 25, 12, 9, 2.8f), 1);
+
+        _debugBattleSquads.Add(new BattleSquadModel(playerUnit));
+        _debugBattleSquads.Add(new BattleSquadModel(allySquadOne));
+        _debugBattleSquads.Add(new BattleSquadModel(allySquadTwo));
+        _debugBattleSquads.Add(new BattleSquadModel(enemyUnit));
+
+        Debug.Log("Debug battle squads created.");
+    }
+
+    private static UnitDefinitionSO CreateDebugDefinition(
+        string unitName,
+        UnitType type,
+        int levelIndex,
+        int health,
+        int damage,
+        int defense,
+        int initiative,
+        float speed)
+    {
+        var definition = ScriptableObject.CreateInstance<UnitDefinitionSO>();
+        definition.UnitName = unitName;
+        definition.Type = type;
+        definition.Levels = new List<UnitLevelDefintion>
+        {
+            new()
+            {
+                LevelIndex = levelIndex,
+                XPToNext = 100,
+                Health = health,
+                Damage = damage,
+                Defense = defense,
+                Initiative = initiative,
+                Speed = speed
+            }
+        };
+
+        return definition;
     }
 
     private void OnDestroy()
