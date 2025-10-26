3e4d31f30ebb3fd6c718afb13346dd9edf262b1d
thisman <tablescable@gmail.com>
2025-10-26 22:49:12 +0500

    Refactoring

--- Assets/_Project/Scripts/Gameplay/Mechanics/DefaultBattleDamageResolver.cs
+++ Assets/_Project/Scripts/Gameplay/Mechanics/DefaultBattleDamageResolver.cs
@@ -5,11 +5,6 @@ public sealed class DefaultBattleDamageResolver : IBattleDamageResolver
 {
     public async Task ResolveDamage(BattleSquadController actor, BattleSquadController target)
     {
-        if (actor == null)
-            throw new ArgumentNullException(nameof(actor));
-        if (target == null)
-            throw new ArgumentNullException(nameof(target));
-
         int damage = actor.ResolveDamage();
         target.ApplyDamage(damage);
 
