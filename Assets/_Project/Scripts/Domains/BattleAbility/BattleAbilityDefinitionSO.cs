a3067a2d76b0df4d77d92f4234b099f00f9fc0f2
thisman <tablescable@gmail.com>
2025-10-28 00:39:55 +0500

    Add base battle ability definition

---
+++ Assets/_Project/Scripts/Domains/BattleAbility/BattleAbilityDefinitionSO.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BattleAbilityType
+{
+    Active,
+    Passive,
+}
+
+public enum BattleAbilityTargetType
+{
+    SingleEnemy,
+    AllEnemies,
+    Self,
+    Ally,
+    AllAllies,
+}
+
+public abstract class BattleAbilityDefinitionSO : ScriptableObject
+{
+    public string Id;
+
+    public string AbilityName;
+
+    public string Description;
+
+    public Sprite Icon;
+
+    public int Cooldown;
+
+    public bool IsReady;
+
+    public BattleAbilityType AbilityType;
+
+    public BattleAbilityTargetType AbilityTargetType;
+
+    public abstract void Apply(BattleContext ctx, IReadOnlySquadModel user, IReadOnlyList<IReadOnlySquadModel> targets);
+}
