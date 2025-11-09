4c2cc9bf8c037d995fe67e5c89d26f18293fd378
Алексей Леготкин <tablescable@gmail.com>
2025-11-09 22:08:54 +0500

    Add breathing scale animation to player

--- Assets/Project/Scripts/Domains/Player/PlayerAnimationController.cs
+++ Assets/Project/Scripts/Domains/Player/PlayerAnimationController.cs
@@ -6,12 +6,17 @@ public class PlayerAnimationController : MonoBehaviour
     [SerializeField] private SpriteRenderer _spriteRenderer;
     [SerializeField] private Rigidbody2D _rigidbody;
     [SerializeField] private float _movementThreshold = 0.01f;
+    [SerializeField] private float _scaleAmplitude = 0.1f;
+    [SerializeField] private float _scaleFrequency = 2f;
 
     [Inject] private readonly GameSession _gameSession;
+
+    private Vector3 _initialScale;
 
     private void Start()
     {
         _spriteRenderer.sprite = _gameSession.HeroDefinition.Icon;
+        _initialScale = transform.localScale;
     }
 
     private void Update()
@@ -29,5 +34,14 @@ public class PlayerAnimationController : MonoBehaviour
         {
             _spriteRenderer.flipX = false;
         }
+
+        AnimateScale();
+    }
+
+    private void AnimateScale()
+    {
+        var scaleOffset = Mathf.Sin(Time.time * _scaleFrequency) * _scaleAmplitude;
+        var targetScale = _initialScale * (1f + scaleOffset);
+        transform.localScale = targetScale;
     }
 }
