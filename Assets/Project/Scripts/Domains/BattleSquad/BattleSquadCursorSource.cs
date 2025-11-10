4088551a587213049d06c0e8d870c8474dc859d6
Алексей Леготкин <tablescable@gmail.com>
2025-11-10 20:52:54 +0500

    Refine battle squad target cursor handling

---
+++ Assets/Project/Scripts/Domains/BattleSquad/BattleSquadCursorSource.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public sealed class BattleSquadCursorSource : ICursorSource
+{
+    [SerializeField] private string _targetCursorName;
+    [SerializeField] private string _defaultCursorName;
+
+    private BattleSquadController _controller;
+
+    private void Awake()
+    {
+        _controller ??= GetComponent<BattleSquadController>();
+    }
+
+    public override CursorSourceData GetCursorState()
+    {
+        return new CursorSourceData
+        {
+            Cursor = ResolveCursorName()
+        };
+    }
+
+    private string ResolveCursorName()
+    {
+        var controller = _controller ??= GetComponent<BattleSquadController>();
+        if (controller != null && controller.IsValidTarget())
+            return ResolveTargetCursor();
+
+        return ResolveDefaultCursor();
+    }
+
+    private string ResolveTargetCursor()
+    {
+        if (!string.IsNullOrWhiteSpace(_targetCursorName))
+            return _targetCursorName;
+
+        return ResolveDefaultCursor();
+    }
+
+    private string ResolveDefaultCursor()
+    {
+        if (!string.IsNullOrWhiteSpace(_defaultCursorName))
+            return _defaultCursorName;
+
+        return CursorOnHover;
+    }
+}
