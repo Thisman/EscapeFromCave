35723aa295887994116c3242bb0364a39e2670d5
thisman <tablescable@gmail.com>
2025-11-17 06:38:22 +0500

    Require components for Object scripts

--- Assets/Scripts/Entities/Objects/ObjectAnimationController.cs
+++ Assets/Scripts/Entities/Objects/ObjectAnimationController.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(ObjectController))]
 public class ObjectAnimationController : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer _spriteRenderer;
