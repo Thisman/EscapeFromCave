3d4e6cb0cf441aa7a066fd2c2abbae3282f9c872
thisman <tablescable@gmail.com>
2025-11-18 21:57:04 +0500

    Default progressions template

--- Assets/Scripts/Entities/Unit/UnitProgressionTemplateSO.cs
+++ Assets/Scripts/Entities/Unit/UnitProgressionTemplateSO.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 public sealed class UnitProgressionTemplateSO : ScriptableObject
 {
     [Min(1)] public float BaseHealth = 100f;
-    [Range(0, 1)] public float BasePhysicalDefense = 2f;
-    [Range(0, 1)] public float BaseMagicDefense = 0f;
-    [Range(0, 1)] public float BaseAbsoluteDefense = 0f;
+    [Min(0)] public float BasePhysicalDefense = 2f;
+    [Min(0)] public float BaseMagicDefense = 0f;
+    [Min(0)] public float BaseAbsoluteDefense = 0f;
     [Min(0)] public float MinDamage = 10f;
     [Min(0)] public float MaxDamage = 20f;
-    [Min(1)] public float Speed = 2f;
-    [Range(0, 1)] public float BaseCritChance = 1f;
-    [Min(1)] public float BaseCritMultiplier = 1.1f;
-    [Range(0, 1)] public float BaseMissChance = 5f;
+    [Min(0)] public float Speed = 2f;
+    public float BaseCritChance = 1f;
+    [Min(0)] public float BaseCritMultiplier = 1.1f;
+    public float BaseMissChance = 5f;
 }
